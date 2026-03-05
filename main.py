1b202b5701d9aa823770138eb91805ca4b5fc410
Lorenzo Sacerdote <lsacerdote@itba.edu.ar>
2026-03-05 18:22:56 -0300

    commit 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 def tirada(cant_dados):
     resultado=[]
-    for i in range(cant_dados)
+    for i in range(cant_dados):
         resultado.append(random.randint(1,6))            
     return resultado
     
@@ -14,6 +14,9 @@ def numero_elegido(dados, numero):
 
 def jugadas(dados):
     repeticiones=[]
+    trio=False
+    par=False
+    
     for i in range(1,7):
         repeticiones=dados.count(i)
         if repeticiones==5:
@@ -24,21 +27,33 @@ def jugadas(dados):
             trio=True
         elif repeticiones==2:
             par=True
-    if dados = [1,2,3,4,5] or dados == [2,3,4,5,6]:
+    if sorted(dados) == [1,2,3,4,5] or sorted(dados) == [2,3,4,5,6]:
             return "Escalera"
     if trio and par:
         return "Full"
     return "Nada"
     
-def puntaje(jugada):
-    if jugada == "Generala":
-        return 50
-    elif jugada == "Poker":
-        return 40
-    elif jugada == "Full":
-        return 30
-    elif jugada == "Escalera":
-        return 20
-    else:
-        return 0
+def puntaje(jugada_detectada, nro_tiro, categoria_elegida, dados):    
+    # 1. Si elige un número (1 al 6)
+    if categoria_elegida.isdigit() and nro_tiro==3:
+        return numero_elegido(dados, int(categoria_elegida))
+    
+    # 2. Si elige una jugada especial
+    if categoria_elegida == "G" and jugada_detectada == "Generala":
+        return 80 if nro_tiro == 1 else 50
+    
+    if categoria_elegida == "P" and jugada_detectada == "Poker":
+        return 45 if nro_tiro == 1 else 40
+    
+    if categoria_elegida == "F" and jugada_detectada == "Full":
+        return 35 if nro_tiro == 1 else 30
+    
+    if categoria_elegida == "E" and jugada_detectada == "Escalera":
+        return 25 if nro_tiro == 1 else 20
+        
+    # 3. Jugada Obligatoria: Si elige algo que no tiene o no es nada
+    return 0
+    
+
+
 
