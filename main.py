3bbfe3885634ef5b18431c279fa87b4db4fa8438
Lorenzo Sacerdote <lsacerdote@itba.edu.ar>
2026-03-05 17:54:58 -0300

    commit 1

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-# Tu implementacion va aqui
-def hola_mundo():
-    return "hola_mundo"
+import random
+def tirada(cant_dados):
+    resultado=[]
+    for i in range(cant_dados)
+        resultado.append(random.randint(1,6))            
+    return resultado
+    
+def numero_elegido(dados, numero):
+    suma = 0
+    for dado in dados:
+        if dado == numero:
+            suma += numero
+    return suma
 
+def jugadas(dados):
+    repeticiones=[]
+    for i in range(1,7):
+        repeticiones=dados.count(i)
+        if repeticiones==5:
+            return "Generala"
+        elif repeticiones==4:
+            return "Poker"
+        elif repeticiones==3:
+            trio=True
+        elif repeticiones==2:
+            par=True
+    if dados = [1,2,3,4,5] or dados == [2,3,4,5,6]:
+            return "Escalera"
+    if trio and par:
+        return "Full"
+    return "Nada"
+    
+def puntaje(jugada):
+    if jugada == "Generala":
+        return 50
+    elif jugada == "Poker":
+        return 40
+    elif jugada == "Full":
+        return 30
+    elif jugada == "Escalera":
+        return 20
+    else:
+        return 0
 
-def main():
-    # Aqui ejecutas tus soluciones
-    print(hola_mundo())
-
-
-# No cambiar a partir de aqui
-if __name__ == "__main__":
-    main()
